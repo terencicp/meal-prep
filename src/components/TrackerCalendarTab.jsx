21a2cbe6099635025609b2b0b1e1ea00ba50fb57
Terenci Claramunt <47529793+terencicp@users.noreply.github.com>
2026-05-27 11:59:55 +0200

    Version up to 0.4.2

--- src/components/TrackerCalendarTab.jsx
+++ src/components/TrackerCalendarTab.jsx
@@ -216,7 +216,7 @@ export default function TrackerCalendarTab({
         </div>
       </div>
       <div className='mt-8 text-center text-xs font-semibold text-gray-400'>
-        v0.4.1
+        v0.4.2
       </div>
     </div>
   );
