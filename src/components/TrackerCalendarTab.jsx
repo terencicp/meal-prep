016095253d8871d9cca85afafeaad39768f8fae4
Terenci Claramunt <47529793+terencicp@users.noreply.github.com>
2026-05-30 13:58:44 +0200

    Increase iOS icon resolution

--- src/components/TrackerCalendarTab.jsx
+++ src/components/TrackerCalendarTab.jsx
@@ -216,7 +216,7 @@ export default function TrackerCalendarTab({
         </div>
       </div>
       <div className='mt-8 text-center text-xs font-semibold text-gray-400'>
-        v0.4.4
+        v0.4.5
       </div>
     </div>
   );
